d05cfc65ac43fb891035ed52d198d75b42450457
Daniel Livingston <daniel.livingston@live.com>
2024-12-01 15:19:25 -0500

    Add Day 1

---
+++ README.md
@@ -0,0 +1,4 @@
+# Advent of Code 2024
+
+Rust-based implementation of the 2024 Advent of Code.
+Solutions are in `src/days/`.
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use advent2024::days::day1;
+
+fn main() {
+    // TODO: put this into a convenience method once more days are added
+    println!("DAY 1");
+    println!("----------------------------------------");
+    println!("  Part 1: distance   = {}", day1::part1());
+    println!("  Part 2: similarity = {}", day1::part2());
+}
