624c00608f4c88deb0dc9b0bb15a96b8601f3e6b
Daniel Livingston <daniel.livingston@amd.com>
2024-12-02 13:25:20 -0500

    Day 2, Part 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+pub trait AdventSolution {
+    fn part1(&self) -> u32;
+    fn part2(&self) -> u32;
+}
+
 pub mod days {
     pub mod day1;
+    pub mod day2;
 }
+
+pub const CALENDAR: [&dyn AdventSolution; 2] = [
+    &days::day1::Day1,
+    &days::day2::Day2
+];
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use advent2024::days::day1;
+use advent2024::CALENDAR;
 
 fn main() {
-    // TODO: put this into a convenience method once more days are added
-    println!("DAY 1");
-    println!("----------------------------------------");
-    println!("  Part 1: distance   = {}", day1::part1());
-    println!("  Part 2: similarity = {}", day1::part2());
+    CALENDAR.iter().enumerate().rev().for_each(|(day, soln)| {
+        println!("----------------- DAY {} -----------------", day + 1);
+        println!("  Part 1: {}", soln.part1());
+        println!("  Part 2: {}", soln.part2());
+    });
 }
